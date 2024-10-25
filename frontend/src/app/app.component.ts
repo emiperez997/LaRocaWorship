b24231ffb3ff959dfefbf96ad68a084483a76cf1
Emiliano Perez <emi.perez997@gmail.com>
2024-10-25 18:15:35 -0300

    [ADD] Sidenav

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToolbarModule } from './components/toolbar/toolbar.module';
+import { MatDrawer, MatDrawerContainer } from '@angular/material/sidenav';
+import { ToolbarModule } from './shared/components/toolbar/toolbar.module';
+import { SidenavModule } from './shared/components/sidenav/sidenav.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarModule],
+  imports: [
+    RouterOutlet,
+    ToolbarModule,
+    MatDrawerContainer,
+    MatDrawer,
+    SidenavModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ frontend/src/app/shared/components/sidenav/sidenav.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+interface ListItem {
+  name: string;
+  icon: string;
+  url: string;
+}
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.scss',
+})
+export class SidenavComponent {
+  @Output() toggleEvent = new EventEmitter<void>();
+
+  listItems: ListItem[] = [
+    {
+      name: 'Inicio',
+      icon: 'home',
+      url: '/',
+    },
+    {
+      name: 'Canciones',
+      icon: 'library_music',
+      url: '/songs',
+    },
+    {
+      name: 'Artistas',
+      icon: 'person',
+      url: '/artistas',
+    },
+    {
+      name: 'Inicio de sesión',
+      icon: 'login',
+      url: '/login',
+    },
+  ];
+}
